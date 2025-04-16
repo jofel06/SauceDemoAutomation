0354b3c537e8c534ad54a9bd7944adf14be72d8e
Christian Nathorst <jofel.nathorst@gmail.com>
2025-04-16 21:27:22 +0800

    Some fixes with features

--- src/test/java/base/Hooks.java
+++ src/test/java/base/Hooks.java
@@ -3,18 +3,22 @@ package base;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import utils.ConfigReader;
 import io.cucumber.java.Scenario;
 
+
 public class Hooks {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @Before
-    public void setupWebDriver(Scenario scenario) { // Added Scenario argument
+    public void setupWebDriver(Scenario scenario) {
         String browserName = ConfigReader.getBrowser();
+        Allure.step("Running test on " + browserName + " browser");
+
         switch (browserName.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
@@ -29,14 +33,14 @@ public class Hooks {
         }
         getDriver().get(ConfigReader.getUrl());
         getDriver().manage().window().maximize();
-        System.out.println("Starting scenario: " + scenario.getName());
+        System.out.println("Starting scenario: " + scenario.getName() + "on browser: " + browserName);
     }
 
     @After
     public void quitWebDriver(Scenario scenario) { // Added Scenario argument
         if (getDriver() != null) {
             getDriver().quit();
-            driver.remove(); //this wil cleanup Thread Local
+            driver.remove();
         }
         System.out.println("Ending scenario: " + scenario.getName());
     }
--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -3,9 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
-@Test
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = { "steps", "base" }, // Package containing the step definitions and hooks
